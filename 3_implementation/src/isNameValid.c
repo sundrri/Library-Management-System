92990ec8aeff7b1a6f9dc265104696f68882d8d8
shrishti-1309 <69230664+shrishti-1309@users.noreply.github.com>
2021-07-07 12:14:00 +0530

    Create isNameValid.c

---
+++ 3_implementation/src/isNameValid.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+int isNameValid(const char *name)
+{
+    int validName = 1;
+    int len = 0;
+    int index = 0;
+    len = strlen(name);
+    for(index =0; index <len ; ++index)
+    {
+        if(!(isalpha(name[index])) && (name[index] != '\n') && (name[index] != ' '))
+        {
+            validName = 0;
+            break;
+        }
+    }
+    return validName;
+}
