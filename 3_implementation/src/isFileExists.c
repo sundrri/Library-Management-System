e16f0a2d9ace603736fc24a64383c646dec3aa6f
shrishti-1309 <69230664+shrishti-1309@users.noreply.github.com>
2021-07-07 12:02:03 +0530

    Create isFileExists.c

---
+++ 3_implementation/src/isFileExists.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+int isFileExists(const char *path)
+{
+    // Try to open file
+    FILE *fp = fopen(path, "rb");
+    int status = 0;
+    // If file does not exists
+    if (fp != NULL)
+    {
+        status = 1;
+        // File exists hence close file
+        fclose(fp);
+    }
+    return status;
+}
