9c26533f9d3c090754e79c533ad03c82cdfdd613
shrishti-1309 <69230664+shrishti-1309@users.noreply.github.com>
2021-07-07 12:01:12 +0530

    Create init.c

---
+++ 3_implementation/src/init.c
@@ -0,0 +1,24 @@
+#include "header.h"
+
+void init()
+{
+    FILE *fp = NULL;
+    int status = 0;
+    const char defaultUsername[] ="admin\n";
+    const char defaultPassword[] ="admin\n";
+    sFileHeader fileHeaderInfo = {0};
+    status = isFileExists(FILE_NAME);
+    if(!status)
+    {
+        //create the binary file
+        fp = fopen(FILE_NAME,"wb");
+        if(fp != NULL)
+        {
+            //Copy default password
+            strncpy(fileHeaderInfo.password,defaultPassword,sizeof(defaultPassword));
+            strncpy(fileHeaderInfo.username,defaultUsername,sizeof(defaultUsername));
+            fwrite(&fileHeaderInfo,FILE_HEADER_SIZE, 1, fp);
+            fclose(fp);
+        }
+    }
+}
